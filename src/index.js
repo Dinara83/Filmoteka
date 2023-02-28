08993ad60e13d672cafd904687c65eb73015294b
Dinara83 <mavleeva.dinara@gmail.com>
2023-02-28 22:49:20 +0200

    formatings

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import './js/search-movies';
 import './sass/index.scss';
 
 import 'basiclightbox/dist/basicLightbox.min.css';
-import { onFormSubmit } from './js/search-movies';
+// import { onFormSubmit } from './js/search-movies';
 import { btnUp } from './js/button-up';
 import './js/firebase-module';
 btnUp.addEventListener();
